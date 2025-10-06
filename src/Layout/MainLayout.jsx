90409b3bab0ffde471e3d003f4d4cc95345024c6
ShukurulloM <muhammadovshukurullo3@gmail.com>
2025-10-06 10:09:45 +0500

    loading commit

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../Components/Header';
 import { Outlet } from 'react-router-dom';
 import Footer from '../Components/Footer';
+import { useState } from 'react';
 
 const MainLayout = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const timer = setLoading(() => {
+      setLoading(false)
+    }, 1000);
+    return () => clearTimeout(timer)
+  })
+  if (loading) {
+    <div className="fixed inset-0 flex items-center justify-center bg-white z-50">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-[#ec5714]"></div>
+    </div>
+  }
   return (
     <div className='w-full min-h-screen flex flex-col bg-picture text-white'>
-      <Header/>
+      <Header />
       <main className='grow'>
-        <Outlet/>
+        <Outlet />
       </main>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
